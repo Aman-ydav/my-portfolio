6760ed64418ec1803f4ffb8070e7eac2d7c7dd01
Aman <amanyadav923949@gmail.com>
2025-07-22 16:24:07 +0530

    heroImg

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,33 @@
-import BackGround1 from '../assets/bg1.png';
-import BackGround2 from '../assets/bg2.png';
-
+import BackGround1 from "../assets/bg1.png";
+import BackGround2 from "../assets/bg2.png";
+import "./HomePage.css";
 function HomePage() {
   return (
-    <section className="h-auto w-full bg-blue-950" id="home">
-      <img className='w-full'  src={BackGround1} alt="" />
-      <img className='w-full'  src={BackGround2} alt="" />
-      <img className='w-full'  src={BackGround1} alt="" />
-    </section>
+    <>
+      <section className="relative w-full min-h-screen bg-[#0e0e21] text-white overflow-hidden">
+        <div className="absolute top-[-100px] left-[-100px] w-[500px] h-[300px] bg-purple-400 opacity-50 blur-[150px] rounded-full z-0 pointer-events-none" />
+        {/* <div className="absolute top-[-100px] right-[-100px] w-[300px] h-[200px] bg-purple-400 opacity-50 blur-[150px] rounded-full z-0 pointer-events-none" /> */}
+        <div className="absolute inset-0 bg-grid-pattern mask-gradient-bottom z-[0] pointer-events-none" />
+
+        <div className="relative z-10 flex flex-col justify-center items-center text-center pt-20 px-4">
+          <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Transforming Concepts into Seamless User Experiences
+          </h1>
+          <p className="mt-4 text-lg text-gray-300">
+            Hi, I'm Aman, a Software Developer based in India.
+          </p>
+          <button className="mt-6 px-6 py-2 border border-white rounded-lg hover:bg-white hover:text-black transition-all">
+            Show my work →
+          </button>
+        </div>
+      </section>
+
+      <section className="relative w-full min-h-screen bg-[#0e0e21] text-white">
+        <div className="absolute inset-0 z-0 "></div>
+        <div className="absolute inset-0 mask-gradient-bottom z-[0] pointer-events-none" />
+      </section>
+    </>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
   const [isScrolled,setIsScrolled] = useState(false);
   useEffect(()=>{
     const onscroll = ()=>{
-      setIsScrolled(window.scrollY>50)
+      setIsScrolled(window.scrollY>30)
     }
     window.addEventListener("scroll",onscroll);
     return ()=>window.removeEventListener("scroll",onscroll); // clean up ke liye, baad me ye dobara na chl jaye 
   },[]);
   return (
     <header className={`h-17 w-full flex justify-between fixed top-0 left-0 z-50 transition-colors duration-300 ${
-        isScrolled ? "bg-black/90 backdrop-blur-md shadow-md" : "bg-transparent"
+        isScrolled ? "bg-[#0f0f0f]/40 backdrop-blur-sm shadow-sm border-b border-white/1" : "bg-transparent"
       }`}>
         <div className="logo flex justify-evenly flex-1">
             <a href="./" className="clickBounce" onClick={(e) => e.preventDefault()}><img className="h-18 w-20 logo-bounce cursor-pointer" src={logoDark} alt="logo" /></a>
